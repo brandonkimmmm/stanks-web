46837bd6cfff2941bffd4ed3ded6411a1285520f
Brandon Kim <bkim2490@gmail.com>
2019-11-12 22:56:55 +0900

    add lanindg and rename players

---
+++ src/Components/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Col, Row, Jumbotron } from 'react-bootstrap';
+import PlayerList from './PlayerList';
+
+function Landing() {
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<Jumbotron>
+						<h1>Stanks</h1>
+						<p>
+							Welcome to Stanks! We have all the unnecessary stats that you never asked for!
+						</p>
+					</Jumbotron>
+				</Col>
+				<Col>
+					<PlayerList />
+				</Col>
+			</Row>
+		</Container>
+	)
+}
+
+export default Landing;
