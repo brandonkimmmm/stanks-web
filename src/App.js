b4af6dbb31e5a6d9b7a47050ebd9c8cf7a2b3dfc
Brandon Kim <bkim2490@gmail.com>
2019-11-17 11:43:16 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Landing from './Components/Landing';
+import Player from './Components/Player';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<Landing />
+			<Switch>
+				<Route exact path='/' component={Landing} />
+				<Route path='/player/:first/:last' component={Player} />
+			</Switch>
 		</div>
 	);
 }
